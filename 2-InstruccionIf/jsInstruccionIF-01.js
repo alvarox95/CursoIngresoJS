1f0f0d6eda741339e7d272888747efdb99c64465
alvarox95 <99840908+alvarox95@users.noreply.github.com>
2023-02-02 23:03:48 -0300

    IF CLASE 2

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = txtIdEdad.value;
+
+	if (edad == 15){
+		alert("Niña Bonita")
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,12 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
-	
+	let edad;
+
+	edad = txtIdEdad.value
+
+	if (edad >= 18){
+		alert("Es mayor de Edad")
+	}
 
 }//FIN DE LA FUNCIÓN
